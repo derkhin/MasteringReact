502efdf2b4b0ee760c312ca202cd3bece77628fb
derkhin <mr.sayyedirfan@gmail.com>
2023-08-02 19:55:55 +0530

    added parcel and .gitignore file

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-{/* <div>
-    <div id="child1">
-        <h1>h1 test child1</h1>
-        <h2>h2 test child1</h2>
-    </div>
-    <div id="child2">
-        <h1>h1 test child2</h1>
-        <h2>h2 test child2</h2>
-    </div>
-</div> */}
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const struct = React.createElement('div', {}, [
     React.createElement('div', { id: 'child1' }, [
-        React.createElement('h1', {}, "h1 test child1"),
+        React.createElement('h1', {}, "h1 test child1 🚀"),
         React.createElement('h2', {}, "h2 test child1")
     ]),
     React.createElement('div', { id: 'child2' }, [
-        React.createElement('h1', {}, "h1 test child2"),
+        React.createElement('h1', {}, "h1 test child3"),
         React.createElement('h2', {}, "h2 test child2")
     ])
 ])
