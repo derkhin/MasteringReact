bc2a95501b94c194e58d3ce6cec3b989471a7131
Irfan <irfan@saaragh.in>
2023-08-04 18:28:44 +0530

    component composition

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const struct = React.createElement('div', {}, [
-    React.createElement('div', { id: 'child1' }, [
-        React.createElement('h1', {}, "h1 test child1 🚀"),
-        React.createElement('h2', {}, "h2 test child1")
-    ]),
-    React.createElement('div', { id: 'child2' }, [
-        React.createElement('h1', {}, "h1 test child3"),
-        React.createElement('h2', {}, "h2 test child2")
-    ])
-])
-
+const heading = React.createElement("h1", { id: "heading" }, "testting 🚀")
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(struct);
+
+// using jsx -babel transpiling html like syntax which is jsx to react element 
+
+const JSXHeading = (<h1 id='heading'>
+    jsx heading 🚀
+</h1>)
+
+
+// component introduction 
+const Title = () => {
+    return (
+        <h1 id='heading'>Title From Functional Component</h1>
+    )
+}
+
+
+// this is also known as component composition
+// component inside component
+const Heading = () => (
+    <div>
+        <Title />
+        <h1>Heading From Functional Component</h1>
+    </div>
+)
+
+
+root.render(<Heading />);
